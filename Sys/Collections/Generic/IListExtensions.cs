74f9142301185ed448c1ea8d8c3e85d0e511b288
benmccallum <ben.mccallum@live.com.au>
2014-09-11 15:51:51 +1000

    Update IListExtensions.cs
    Removed Shuffle method from IListExtensions as it wasn't truly random.

--- Sys/Collections/Generic/IListExtensions.cs
+++ Sys/Collections/Generic/IListExtensions.cs
@@ -5,25 +5,6 @@ using System.Linq;
 public static class IListExtensions
 {
     /// <summary>
-    /// Randomly shuffles the items in the IList.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="list"></param>
-    public static void Shuffle<T>(this IList<T> list)
-    {
-        Random rng = new Random();
-        int n = list.Count;
-        while (n > 1)
-        {
-            n--;
-            int k = rng.Next(n + 1);
-            T value = list[k];
-            list[k] = list[n];
-            list[n] = value;
-        }
-    }
-
-    /// <summary>
     /// Splits a large list into a list of smaller IEnumerable objects of a max list size given.
     /// </summary>
     /// <typeparam name="T"></typeparam>
