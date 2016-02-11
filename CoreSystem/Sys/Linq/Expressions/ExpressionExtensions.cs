3b187fb1dec347532225ff6ebaaf44dafe7e9912
Ben McCallum <ben.mccallum@live.com.au>
2016-02-12 10:46:23 +1100

    Added Inverse Expression method
    Also, should start doing SemVer, not matter how small the feature.

---
+++ CoreSystem/Sys/Linq/Expressions/ExpressionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq.Expressions;
+
+public static class ExpressionExtensions
+{
+    public static Expression<Func<T, bool>> Inverse<T>(this Expression<Func<T, bool>> e)
+    {
+        return Expression.Lambda<Func<T, bool>>(Expression.Not(e.Body), e.Parameters[0]);
+    }
+}
